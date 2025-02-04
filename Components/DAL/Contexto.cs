19de57c613541064458e3d05657435a01f53873c
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-03 21:13:06 -0400

    Cree y modifique el contexto

--- Components/DAL/Contexto.cs
+++ Components/DAL/Contexto.cs
@@ -1,6 +1,15 @@
-﻿namespace Francis_Castillo_P1_AP1.Components.DAL
+﻿using Francis_Castillo_P1_AP1.Components.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Francis_Castillo_P1_AP1.Components.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<ParcialModelo> Parcial { get; set; }
+
     }
 }
