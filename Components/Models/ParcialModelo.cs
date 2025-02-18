ffb33b296030943bf329cad3cf74a585ac3e4a68
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-17 21:02:42 -0400

    Todos los cambios hasta el momento

--- Components/Models/ParcialModelo.cs
+++ Components/Models/ParcialModelo.cs
@@ -8,20 +8,22 @@ public class ParcialModelo
 
         [Key]
         public int AporteId { get; set; }
-        [Required(ErrorMessage = "Este campo es requerido")]
 
-        public string Persona { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Este campo es requerido")]
 
+        [Required(ErrorMessage = "Este campo es requerido")]
+         public string Persona { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Observacion { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Este campo es requerido")]
 
+        [Required(ErrorMessage = "Este campo es requerido")]
         public DateTime Fecha { get; set; }
-        [Required(ErrorMessage = "Este campo es requerido")]
 
+        [Required(ErrorMessage = "Este campo es requerido")]
         public double Monto { get; set; }
-        [Required(ErrorMessage = "Este campo es requerido")]
 
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.")]   
         public int AportesId { get; set; }
     
 }
---
+++ Migrations/20250218010118_ModeloNuevo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Francis_Castillo_P1_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModeloNuevo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
