f713d4e602ed65f22d4c6081b7a08180f8898f1c
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-17 20:10:12 -0400

    inyecte el services

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
-using Francis_Castillo_P1_AP1.DAL; // Se corrigió la referencia a DAL
+using Francis_Castillo_P1_AP1.DAL; 
 using Francis_Castillo_P1_AP1.Components;
+using Francis_Castillo_P1_AP1.Services; 
 using Microsoft.EntityFrameworkCore;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
-// Add services to the container.
+
+builder.Services.AddScoped<AportesService>();
+
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
