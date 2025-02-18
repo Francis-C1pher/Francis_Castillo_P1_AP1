b8da77fe6f19da4c68212b1ae7d06c396deaa7da
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-17 23:05:20 -0400

    Modifique el modelo, asi que  cree una nueva migracion y actualize la base de datos

--- Components/Models/ParcialModelo.cs
+++ Components/Models/ParcialModelo.cs
@@ -1,36 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
+﻿using System.ComponentModel.DataAnnotations;
 namespace Francis_Castillo_P1_AP1.Components.Models;
 
 public class ParcialModelo
 {
+    [Key]
+    public int AporteId { get; set; }
 
-        [Key]
-        public int AporteId { get; set; }
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string Persona { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string Observacion { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Este campo es requerido")]
-         public string Persona { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
 
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public string Observacion { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public DateTime Fecha { get; set; }
-
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public double Monto { get; set; }
-
-        [Required(ErrorMessage = "Este campo es requerido")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.")]   
-        public int AportesId { get; set; }
-    
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.")]
+    public double Monto { get; set; }
 }
-        
-       
-    
-   
-
-
-
--- Migrations/20250218020113_BorrandoCampoInnecesario.Designer.cs
+++ Migrations/20250218020113_BorrandoCampoInnecesario.Designer.cs
@@ -4,6 +4,7 @@ using Francis_Castillo_P1_AP1.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Francis_Castillo_P1_AP1.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250218020113_BorrandoCampoInnecesario")]
+    partial class BorrandoCampoInnecesario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
